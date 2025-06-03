46b4a34f222ff4dd6f2a7c6614299428ab97fb6d
Ethan-Toste <60124705+eat7890-stick@users.noreply.github.com>
2025-06-03 00:52:24 -0400

    Live Camera Rough Draft

---
+++ Firmware/Live_Camera_Board/pinouts.h
@@ -0,0 +1,28 @@
+// Header file that contains all pinouts for this module
+#define V_THERM_PIN PB0
+#define UNUSED_PIN PB1
+#define BOOT1_PIN PB2
+#define VTX_PWR_PIN PB3
+#define CAN_RX_PIN PB8
+#define CAN_TX_PIN PB9
+#define VTX_DATA_PIN PB10
+#define FLASH_CS_PIN PB12
+#define FLASH_SCK_PIN PB13
+#define FLASH_MISO_PIN PB14
+#define FLASH_MOSI_PIN PB15
+
+#define RGB_DATA_PIN PA0
+#define VTX_BATT_SENSE_PIN PA1
+#define USB_TX_PIN PA2
+#define USB_RX_PIN PA3
+#define CAMERA_POWER_PIN PA6
+#define BUZZER_PIN_B PA7
+#define BUZZER_PIN_A PA8
+#define CAM_TX_PIN PA9
+#define CAM_RX_PIN PA10
+#define FLASH_RESET_PIN PA13
+#define STATUS_LED_PIN PA15
+
+
+#define MAGN_ADDR 0x0E 
+#define BME680_ADDR 0x77
